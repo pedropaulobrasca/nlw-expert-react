95a82488058b84d43387f6457c3b72194d17032b
Pedro Brasca <pedropaulobrascas@gmail.com>
2024-02-06 13:56:29 -0300

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { App } from './app'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
