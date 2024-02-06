be30bfc7487adc23bc415c30a806e947064c249e
Pedro Brasca <pedropaulobrascas@gmail.com>
2024-02-06 15:04:29 -0300

    feat: finalizado a home page com os cards

---
+++ src/components/note-card.tsx
@@ -0,0 +1,14 @@
+export function NoteCard() {
+  return (
+    <button className="relative space-y-3 overflow-hidden rounded-md bg-slate-800 p-5 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+      <p className="text-sm leading-6 text-slate-400">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem magni
+        temporibus itaque ipsum dolor, ea adipisci inventore qui mollitia
+        architecto porro est sint illum, ab aut molestias impedit vitae autem?
+      </p>
+
+      <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />
+    </button>
+  )
+}
